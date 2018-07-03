75d94050bf23dfa2f63356817ece0660f8197b76
Wyatt Ades <wyattades@gmail.com>
2018-07-03 13:34:40 -0700

    Enforce npm cheerio installed

--- getsite.sh
+++ getsite.sh
@@ -8,6 +8,8 @@ fi
 
 set -e
 
+npm list cheerio > /dev/null
+
 mkdir $1
 wget -o $1/.download.log -e robots=off --debug -N -S --random-wait -x -r -p -l inf -E --convert-links --domains="`echo $1`" $1
 
@@ -15,3 +17,6 @@ for file in $(find $1 -name '*.html'); do
   node replace_extensions.js $file && echo "Parsed $file" &
 done
 wait
+
+echo ""
+echo "Successfully staticified: $1"
